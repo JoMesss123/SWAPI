df25836ad19e0199edebea9b5724e55d9576c2c8
Jonathan Claes <115749108+JoMesss123@users.noreply.github.com>
2024-01-29 16:24:50 -0500

    style 404

--- SWAPI_API/src/views/erreur.vue
+++ SWAPI_API/src/views/erreur.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="erreur">
         <h1>404</h1>
-        <p>Page not found</p>
+        <p>Ne passez pas par le côter lumineux de la Force </p>
     </div>
 </template>
 
@@ -11,4 +11,35 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.erreur{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+   max-width: 1200px;
+    margin: 0 auto;
+    background-image: url("../assets/img/media-75965-1024x576.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100%;
+    text-align: center;
+
+}
+
+h1{
+    font-size: 100px;
+    color: #FF0000;
+    font-family: 'Cinzel', serif;
+}
+
+p{
+    background-color: rgba(0, 0, 0, 0.596);
+    font-size: 50px;
+    color: #FF0000;
+    font-family: 'Cinzel', serif;
+}
+</style>
